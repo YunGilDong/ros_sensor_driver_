cd3bbfd2caefc67c14808e64e88e145c7059d72b
YunGilDong <gdyun@the-road.co.kr>
2019-11-07 18:25:32 -0800

    change network interface

--- network_interface/src/utils.cpp
+++ network_interface/src/utils.cpp
@@ -1,49 +1,31 @@
 /*
-* Unpublished Copyright (c) 2009-2017 AutonomouStuff, LLC, All Rights Reserved.
+* Unpublished Copyright (c) 2009-2019 AutonomouStuff, LLC, All Rights Reserved.
 *
-* This file is part of the network_interface ROS 1.0 driver which is released under the MIT license.
+* This file is part of the network_interface ROS driver which is released under the MIT license.
 * See file LICENSE included with this software or go to https://opensource.org/licenses/MIT for full license details.
 */
 
+#include <network_interface/network_interface.h>
 #include <string>
-#include <network_interface.h>
 
-std::string AS::Network::return_status_desc(const return_statuses &ret)
+std::string AS::Network::return_status_desc(const ReturnStatuses &ret)
 {
-  std::string status_string;
-
-  if (ret == INIT_FAILED)
-  {
-    status_string = "Initialization of the network interface failed.";
-  }
-  else if (ret == BAD_PARAM)
-  {
-    status_string = "A bad parameter was provided to the network interface during initalization.";
-  }
-  else if (ret == SOCKET_ERROR)
-  {
-    status_string = "A socket error was encountered.";
-  }
-  else if (ret == SOCKET_CLOSED)
-  {
-    status_string = "Socket is not currently open.";
-  }
-  else if (ret == NO_MESSAGES_RECEIVED)
-  {
-    status_string = "No messages were received on the interface.";
-  }
-  else if (ret == READ_FAILED)
-  {
-    status_string = "A read operation failed on the network interface.";
-  }
-  else if (ret == WRITE_FAILED)
-  {
-    status_string = "A write operation failed on the network interface.";
-  }
-  else if (ret == CLOSE_FAILED)
-  {
-    status_string = "Closing the network failed.";
-  }
-
-  return status_string;
+  if (ret == ReturnStatuses::INIT_FAILED)
+    return "Initialization of the network interface failed.";
+  else if (ret == ReturnStatuses::BAD_PARAM)
+    return "A bad parameter was provided to the network interface during initalization.";
+  else if (ret == ReturnStatuses::SOCKET_ERROR)
+    return "A socket error was encountered.";
+  else if (ret == ReturnStatuses::SOCKET_CLOSED)
+    return "Socket is not currently open.";
+  else if (ret == ReturnStatuses::NO_MESSAGES_RECEIVED)
+    return "No messages were received on the interface.";
+  else if (ret == ReturnStatuses::READ_FAILED)
+    return "A read operation failed on the network interface.";
+  else if (ret == ReturnStatuses::WRITE_FAILED)
+    return "A write operation failed on the network interface.";
+  else if (ret == ReturnStatuses::CLOSE_FAILED)
+    return "Closing the network failed.";
+  else
+    return "Undefined error.";
 }
